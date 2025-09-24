e8183bb9ebe388fa52024bc9025e8179ca1a516c
Leorodrigs <leorodrigues.job@gmail.com>
2025-09-24 14:12:41 -0300

    alterações nos imports

--- src/assets/components/CharacterDetail.jsx
+++ src/assets/components/CharacterDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
-import { Navbar } from "./navbar";
+import { Navbar } from "./Navbar";
 
 export const CharacterDetail = () => {
     const { id } = useParams();
